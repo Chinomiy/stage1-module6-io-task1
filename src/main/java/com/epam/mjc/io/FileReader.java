a72a26813a980eda9dc60dcb3eefe2adbc9a0bc6
Chinomiy <maruishiwo@gmail.com>
2023-10-30 17:18:08 +0300

    new solut

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -3,29 +3,28 @@ package com.epam.mjc.io;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 
 public class FileReader {
 
     public static Profile getDataFromFile(File file) {
-        String nameLine = "";
-        String ageLine = "";
-        String emailLine = "";
-        String phoneLine = "";
-        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))) {
-            nameLine = bufferedReader.readLine();
-            ageLine = bufferedReader.readLine();
-            emailLine = bufferedReader.readLine();
-            phoneLine = bufferedReader.readLine();
-        } catch (IndexOutOfBoundsException | IOException e) {
-            throw new RuntimeException(e);
+        List<String> datas = new ArrayList<>();
+        String n;
+        int i=0;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file));
+            while ((n = Objects.requireNonNull(bufferedReader).readLine()) != null) {
+                String[] split = n.split(" ");
+                datas.add(i,split[1]);
+                i++;
+            }
+            bufferedReader.close();
+        }catch (IOException e){
+            e.printStackTrace();
         }
-        return new Profile(getProfileValue(nameLine), Integer.parseInt(getProfileValue(ageLine)), getProfileValue(emailLine), Long.parseLong(getProfileValue(phoneLine)));
+        return new Profile(datas.get(0),Integer.parseInt(datas.get(1)),datas.get(2),Long.parseLong(datas.get(3)));
     }
-
-    public static String getProfileValue(String profileValue) {
-        String[] data = profileValue.split(":");
-        return data[data.length - 1].trim();
-    }
-    private FileReader(){}
 }
