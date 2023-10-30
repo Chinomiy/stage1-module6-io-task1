10e92f831b1bf18568d64f98a95271bf4e1305e8
Chinomiy <maruishiwo@gmail.com>
2023-10-30 16:25:12 +0300

    v1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,37 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public static Profile getDataFromFile(File file) {
+        Profile profile = new Profile();
+        try(BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+            String[] data = line.split(":");
+                switch (data[0]) {
+                    case "Name" -> profile.setName(getProfileValue(line));
+                    case "Age" -> profile.setAge(Integer.parseInt(getProfileValue(line)));
+                    case "Email" -> profile.setEmail(getProfileValue(line));
+                    case "Phone" -> profile.setPhone((long) Integer.parseInt(getProfileValue(line)));
+                }
+            }
+        } catch (IOException e) {
+
+        }
+        return profile;
+    }
+
+    public static String getProfileValue(String profileValue) {
+        String[] data = profileValue.split(":");
+        return data[data.length-1].trim();
+    }
+    public static String getProfileKeys(String profileKeys){
+        String[] data = profileKeys.split(":");
+        return data[0].trim();
     }
 }
